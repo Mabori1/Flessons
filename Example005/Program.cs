ec3eb4bc0bd2467efe973ce27b024b9537f0b1cc
Alexey <makeevhome@mail.ru>
2022-08-16 21:56:18 +0300

    Examples005

---
+++ Example005/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите имя пользователя: ");
+        string username = Console.ReadLine();
+        if (username.ToLower != "маша")
+        {
+            Console.WriteLine("Привет, " + username);
+        }
+        else
+        {
+            Console.WriteLine("Ура это же Маша!");
+        }
+    }
+}
