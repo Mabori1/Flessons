66b45c4295515a5aebacc7de0026856c4bcf528d
Alexey Makeev <makeevhome@mail.ru>
2022-08-16 17:30:47 +0300

    ex002

--- ex002/Program.cs
+++ ex002/Program.cs
@@ -1,4 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, 1111World!");
-int i = Console.ReadLine();
-Console.WriteLine("hi ," + i);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Friend! What you name?");
+        string val = Console.ReadLine();
+        if (val == null) return;
+            Console.WriteLine("hi, " + val);
+    }
+ }
